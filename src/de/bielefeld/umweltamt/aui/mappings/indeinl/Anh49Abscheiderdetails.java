348bcc3d565143d1aca7e8b534685c91ac1df3d6
Gerd Genuit <GerdGenuit@users.noreply.github.com>
2010-02-02 12:36:00 +0000

    Auswertungen order by betreibername

--- src/de/bielefeld/umweltamt/aui/mappings/indeinl/Anh49Abscheiderdetails.java
+++ src/de/bielefeld/umweltamt/aui/mappings/indeinl/Anh49Abscheiderdetails.java
@@ -94,16 +94,14 @@ public class Anh49Abscheiderdetails
 		
 		String query = "from Anh49Abscheiderdetails details where details.Anh49Fachdaten.basisObjekt.basisObjektarten.objektart like 'Fettabscheider' "
 			//+"order by details.nenngroesse desc";
-			  +"order by details.Anh49Fachdaten.basisObjekt.inaktiv, details.Anh49Fachdaten.basisObjekt.basisStandort.plz, "
-			  +"details.Anh49Fachdaten.basisObjekt.basisBetreiber"; 
+			  +"order by details.Anh49Fachdaten.basisObjekt.inaktiv, details.Anh49Fachdaten.basisObjekt.basisBetreiber.betrname";
 		Session session = HibernateSessionFactory.currentSession();
 		fettabsch = session.createQuery(query).list();
 		HibernateSessionFactory.closeSession();
 		
 		String query2 = "from Anh49Fachdaten anh49"+
 						" where anh49.basisObjekt.basisObjektarten.objektart like 'Fettabscheider' "
-		 				+"order by anh49.basisObjekt.inaktiv, anh49.basisObjekt.basisStandort.plz, "
-		 				+"anh49.basisObjekt.basisBetreiber"; 
+		 				+"order by anh49.basisObjekt.inaktiv, anh49.basisObjekt.basisBetreiber.betrname";
 		session = HibernateSessionFactory.currentSession();
 		fettabsch2 = session.createQuery(query2).list();
 		HibernateSessionFactory.closeSession();
