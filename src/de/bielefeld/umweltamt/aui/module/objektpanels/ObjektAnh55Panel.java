cbddd76ca66797045dd07a0bd4bd1e218c8c373b
Gerd Genuit <GerdGenuit@users.noreply.github.com>
2010-02-04 13:09:38 +0000

    Verkn�Objekte

--- src/de/bielefeld/umweltamt/aui/module/objektpanels/ObjektAnh55Panel.java
+++ src/de/bielefeld/umweltamt/aui/module/objektpanels/ObjektAnh55Panel.java
@@ -1,11 +1,14 @@
 /*
  * Datei:
- * $Id: ObjektAnh55Panel.java,v 1.2 2009-03-24 12:35:22 u633d Exp $
+ * $Id: ObjektAnh55Panel.java,v 1.3 2010-02-04 13:09:38 u633d Exp $
  * 
  * Erstellt am 10.08.2005 von Gerhard Genuit (u633d)
  * 
  * CVS-Log:
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2009/03/24 12:35:22  u633d
+ * Umstellung auf UTF8
+ *
  * Revision 1.1  2008/06/05 11:38:39  u633d
  * Start AUIK auf Informix und Postgresql
  *
@@ -45,16 +48,26 @@
  */
 package de.bielefeld.umweltamt.aui.module.objektpanels;
 
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFormattedTextField;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 
 //import sun.misc.FormattedFloatingDecimal;
 
@@ -64,8 +77,11 @@ import com.jgoodies.forms.layout.FormLayout;
 
 import de.bielefeld.umweltamt.aui.AUIKataster;
 import de.bielefeld.umweltamt.aui.HauptFrame;
+import de.bielefeld.umweltamt.aui.mappings.basis.BasisObjektverknuepfung;
 import de.bielefeld.umweltamt.aui.mappings.indeinl.Anh55Fachdaten;
 import de.bielefeld.umweltamt.aui.module.ObjektBearbeiten;
+import de.bielefeld.umweltamt.aui.module.common.ObjektChooser;
+import de.bielefeld.umweltamt.aui.module.common.tablemodels.ObjektVerknuepfungModel;
 import de.bielefeld.umweltamt.aui.utils.DoubleField;
 import de.bielefeld.umweltamt.aui.utils.IntegerField;
 import de.bielefeld.umweltamt.aui.utils.LimitedTextArea;
@@ -114,6 +130,16 @@ public class ObjektAnh55Panel extends JPanel{
 	
 	// Daten
 	private Anh55Fachdaten fachdaten = null;
+	
+	
+	// Objektverknuepfer
+	private ObjektVerknuepfungModel objektVerknuepfungModel;
+	private JTable objektverknuepfungTabelle = null;
+	private JButton selectObjektButton = null;
+	private Action verknuepfungLoeschAction;
+	private JPopupMenu verknuepfungPopup;	
+	
+	
 	
 	//Listener
 	private ActionListener editButtonListener;
@@ -182,7 +208,22 @@ public class ObjektAnh55Panel extends JPanel{
 		builder.appendRow("fill:30dlu");
 		builder.append(bemerkungsScroller, 7);
 		
-		JPanel buttonBar = ButtonBarFactory.buildOKBar(getSaveAnh55Button());
+		builder.appendSeparator("Verknüpfte Objekte");
+		builder.appendRow("3dlu");
+		builder.nextLine(2);
+		JScrollPane objektverknuepfungScroller = new JScrollPane(
+				getObjektverknuepungTabelle(),
+				JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		builder.appendRow("fill:100dlu");
+		builder.append(objektverknuepfungScroller, 7);
+		builder.nextLine();
+		
+		JPanel buttonBar = ButtonBarFactory.buildRightAlignedBar(
+				getSelectObjektButton(), getSaveAnh55Button());
+		
+		
+	//	JPanel buttonBar = ButtonBarFactory.buildOKBar(getSaveAnh55Button());
 		builder.append(buttonBar, 7);
 		
 	}
@@ -834,7 +875,167 @@ public class ObjektAnh55Panel extends JPanel{
 		}
 		return aktivchlorCheck;
 	}
-//
+
+	
+	private JTable getObjektverknuepungTabelle() {
+		
+		if (objektVerknuepfungModel == null) {
+			objektVerknuepfungModel = new ObjektVerknuepfungModel(hauptModul
+					.getObjekt());
+	
+			if (objektverknuepfungTabelle == null) {
+				objektverknuepfungTabelle = new JTable(objektVerknuepfungModel);
+			} else {
+				objektverknuepfungTabelle.setModel(objektVerknuepfungModel);
+			}
+			objektverknuepfungTabelle.getColumnModel().getColumn(0)
+					.setPreferredWidth(5);
+			objektverknuepfungTabelle.getColumnModel().getColumn(1)
+					.setPreferredWidth(100);
+			objektverknuepfungTabelle.getColumnModel().getColumn(2)
+					.setPreferredWidth(250);
+	
+			objektverknuepfungTabelle
+					.addMouseListener(new java.awt.event.MouseAdapter() {
+						public void mouseClicked(java.awt.event.MouseEvent e) {
+							if ((e.getClickCount() == 2)
+									&& (e.getButton() == 1)) {
+								Point origin = e.getPoint();
+								int row = getObjektverknuepungTabelle()
+										.rowAtPoint(origin);
+	
+								if (row != -1) {
+									BasisObjektverknuepfung obj = objektVerknuepfungModel
+											.getRow(row);
+									if (obj.getBasisObjektByIstVerknuepftMit().getObjektid().intValue() != hauptModul
+											.getObjekt().getObjektid().intValue())
+										hauptModul
+												.getManager()
+												.getSettingsManager()
+												.setSetting(
+														"auik.imc.edit_object",
+														obj
+																.getBasisObjektByIstVerknuepftMit()
+																.getObjektid()
+																.intValue(),
+														false);
+									else
+										hauptModul
+												.getManager()
+												.getSettingsManager()
+												.setSetting(
+														"auik.imc.edit_object",
+														obj
+																.getBasisObjektByObjekt()
+																.getObjektid()
+																.intValue(),
+														false);
+									hauptModul.getManager().switchModul(
+											"m_objekt_bearbeiten");
+								}
+							}
+						}
+	
+						public void mousePressed(MouseEvent e) {
+							showVerknuepfungPopup(e);
+						}
+	
+						public void mouseReleased(MouseEvent e) {
+							showVerknuepfungPopup(e);
+						}
+					});
+	
+			objektverknuepfungTabelle.getInputMap().put(
+					(KeyStroke) getVerknuepfungLoeschAction().getValue(
+							Action.ACCELERATOR_KEY),
+					getVerknuepfungLoeschAction().getValue(Action.NAME));
+			objektverknuepfungTabelle.getActionMap().put(
+					getVerknuepfungLoeschAction().getValue(Action.NAME),
+					getVerknuepfungLoeschAction());
+		}
+	
+		return objektverknuepfungTabelle;
+	
+	}
+	
+	private void showVerknuepfungPopup(MouseEvent e) {
+		if (verknuepfungPopup == null) {
+			verknuepfungPopup = new JPopupMenu("Objekt");
+			JMenuItem loeschItem = new JMenuItem(getVerknuepfungLoeschAction());
+			verknuepfungPopup.add(loeschItem);
+		}
+	
+		if (e.isPopupTrigger()) {
+			Point origin = e.getPoint();
+			int row = objektverknuepfungTabelle.rowAtPoint(origin);
+	
+			if (row != -1) {
+				objektverknuepfungTabelle.setRowSelectionInterval(row, row);
+				verknuepfungPopup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		}
+	}
+
+	private Action getVerknuepfungLoeschAction() {
+		if (verknuepfungLoeschAction == null) {
+			verknuepfungLoeschAction = new AbstractAction("Löschen") {
+				public void actionPerformed(ActionEvent e) {
+					int row = getObjektverknuepungTabelle().getSelectedRow();
+					if (row != -1
+							&& getObjektverknuepungTabelle().getEditingRow() == -1) {
+						BasisObjektverknuepfung verknuepfung = objektVerknuepfungModel
+								.getRow(row);
+						int answer = JOptionPane
+								.showConfirmDialog(
+										hauptModul.getPanel(),
+										"Soll die Verknüpfung wirklich gelöscht werden?\n"
+												+ "Hinweis: Die Aktion betrifft nur die Verknüpfung, die Objekte bleiben erhalten und können jederzeit neu verknüpft werden.",
+										"Löschen bestätigen",
+										JOptionPane.YES_NO_OPTION);
+						if (answer == JOptionPane.YES_OPTION) {
+							if (objektVerknuepfungModel.removeRow(row)) {
+								hauptModul.getFrame().changeStatus(
+										"Objekt gelöscht.",
+										HauptFrame.SUCCESS_COLOR);
+								AUIKataster.debugOutput("Objekt "
+										+ verknuepfung.getId()
+										+ " wurde gelöscht!",
+										"BetreiberSuchen.removeAction");
+							} else {
+								hauptModul.getFrame().changeStatus(
+										"Konnte das Objekt nicht löschen!",
+										HauptFrame.ERROR_COLOR);
+							}
+						}
+					}
+				}
+			};
+			verknuepfungLoeschAction.putValue(Action.MNEMONIC_KEY, new Integer(
+					KeyEvent.VK_L));
+			verknuepfungLoeschAction.putValue(Action.ACCELERATOR_KEY, KeyStroke
+					.getKeyStroke(KeyEvent.VK_DELETE, 0, false));
+		}
+
+		return verknuepfungLoeschAction;
+	}
+
+	private JButton getSelectObjektButton() {
+		if (selectObjektButton == null) {
+			selectObjektButton = new JButton("Objekt auswählen");
+
+			selectObjektButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					ObjektChooser chooser = new ObjektChooser(hauptModul
+							.getFrame(), fachdaten.getBasisObjekt(),
+							objektVerknuepfungModel);
+					chooser.setVisible(true);
+				}
+			});
+		}
+		return selectObjektButton;
+	}
+	
+	//
 //	private JCheckBox getAbwasseranfallCheck() {
 //		if (abwasseranfallCheck == null) {
 //			abwasseranfallCheck = new JCheckBox("Abwasseranfall");
