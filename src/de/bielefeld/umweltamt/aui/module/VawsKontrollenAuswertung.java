f50123c4cbd260961cc744cde765b0e6919a942c
Ingo Weinzierl <weinzierl.ingo@gmail.com>
2011-02-15 09:54:45 +0000

    Lizenzheader und LGPL v2.1 Lizenztext hinzugefügt.

--- src/de/bielefeld/umweltamt/aui/module/VawsKontrollenAuswertung.java
+++ src/de/bielefeld/umweltamt/aui/module/VawsKontrollenAuswertung.java
@@ -1,3 +1,24 @@
+/**
+ * Copyright 2005-2011, Stadt Bielefeld
+ *
+ * This file is part of AUIK (Anlagen- und Indirekteinleiter-Kataster).
+ *
+ * AUIK is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * AUIK is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with AUIK. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * AUIK has been developed by Stadt Bielefeld and Intevation GmbH.
+ */
+
 /*
  * Datei:
  * $Id: VawsKontrollenAuswertung.java,v 1.1.2.1 2010-11-23 10:25:53 u633d Exp $
