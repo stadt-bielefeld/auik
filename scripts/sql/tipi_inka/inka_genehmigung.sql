53ef6dbcaa2d680c3a3b47b876969cb382ecd16e
Conny Pearce <Conny.Pearce@bielefeld.de>
2012-05-04 14:02:51 +0000

    More sqls for tipi/inka

--- scripts/sql/tipi_inka/inka_genehmigung.sql
+++ scripts/sql/tipi_inka/inka_genehmigung.sql
@@ -1,4 +1,4 @@
-SELECT
+﻿SELECT
   -- Primary Key
   indeinl_genehmigung.objektid::integer		AS genehmigung_nr,
   1::integer					AS genehmigung_version,
@@ -22,18 +22,20 @@ SELECT
 -- TODO: <field>_tog is the new <field>_jn. What does "tog" mean?
   indeinl_genehmigung.befristet::integer	AS befristet_tog,	-- NOT NULL
   indeinl_genehmigung.befristet_bis::date	AS befristet_bis,
-  1::integer 					AS zustands_nr,		-- NOT NULL
-  '1970-01-01'::date 				AS gueltig_von, 	-- NOT NULL
-  NULL::date 					AS ig_stichtag,
-  NULL::date 					AS gueltig_bis,
-  '1970-01-01'::date 				AS erstell_dat,		-- NOT NULL
-  '1970-01-01'::date 				AS aktual_dat,		-- NOT NULL
-  1::integer 					AS versionsnr,		-- NOT NULL
-  NULL::timestamp 				AS zeitstempel
+  
+  -- (some more...) Historisierung
+  1::integer 		AS zustands_nr,		-- NOT NULL
+  '1970-01-01'::date	AS gueltig_von, 	-- NOT NULL
+  NULL::date 		AS ig_stichtag,
+  NULL::date 		AS gueltig_bis,
+  '1970-01-01'::date 	AS erstell_dat,		-- NOT NULL
+  '1970-01-01'::date 	AS aktual_dat,		-- NOT NULL
+  1::integer 		AS versionsnr,		-- NOT NULL
+  NULL::timestamp 	AS zeitstempel
 
-FROM 
-  auik.indeinl_genehmigung LEFT OUTER JOIN auik.basis_objekt
-  ON indeinl_genehmigung.objektid = basis_objekt.objektid
+FROM auik.indeinl_genehmigung
+  LEFT OUTER JOIN auik.basis_objekt
+    ON indeinl_genehmigung.objektid = basis_objekt.objektid
   
 WHERE 
   indeinl_genehmigung._deleted = FALSE AND 
