35f55378b33e525d9a9bd05752d6184db17e3263
Conny Pearce <Conny.Pearce@bielefeld.de>
2012-05-16 14:43:26 +0000

    inka_uebergabestelle ohne indeinl_uebergabestelle :)

--- scripts/sql/tipi_inka/vers2.43_inka_uebergabestelle.sql
+++ scripts/sql/tipi_inka/vers2.43_inka_uebergabestelle.sql
@@ -1,10 +1,10 @@
 ﻿SELECT
   -- Primary / Foreign Key: dea_gemeinde
-  '05711000'::character varying(8)		AS gemeindekennzahl,
-  1::integer					AS gemeinde_ver,
+  '05711000'::character varying(8)	AS gemeindekennzahl,
+  1::integer				AS gemeinde_ver,
   -- Primary Key
-  indeinl_uebergabestelle.objektid::integer	AS uebergabestelle_lfd_nr,
-  1::integer					AS uebergabestelle_ver,
+  indeinl_genehmigung.objektid::integer	AS uebergabestelle_lfd_nr,
+  1::integer				AS uebergabestelle_ver,
 
   -- Historisierung
   '1970-01-01'::date					AS gueltig_von, 	-- NOT NULL
@@ -17,11 +17,11 @@
   -- Daten
   -- Foreign Key: inka_betrieb
   (basis_objekt.betreiberid << 16 | basis_objekt.standortid)::integer
-						AS betrieb_nr,		-- NOT NULL
-  1::integer					AS betrieb_ver,		-- NOT NULL
+					AS betrieb_nr,		-- NOT NULL
+  1::integer				AS betrieb_ver,		-- NOT NULL
   -- Foreign Key: inka_genehmigung
-  indeinl_uebergabestelle.objektid::integer	AS genehmigung_nr,	-- NOT NULL
-  1::integer					AS genehmigung_ver,	-- NOT NULL
+  indeinl_genehmigung.objektid::integer	AS genehmigung_nr,	-- NOT NULL
+  1::integer				AS genehmigung_ver,	-- NOT NULL
   -- Foreign Key: dea_klaeranlage
   -- This is the official definition which seems to be rather wrong...
 --  'Therebedragons!'::character varying(15)			AS anlagen_nr,		-- NOT NULL
@@ -31,22 +31,26 @@
   1::integer							AS klaeranlagen_ver,	-- NOT NULL
 --  'NIKK_BR7'::character varying(20)		AS anl_herkunft,	-- NOT NULL
   -- Foreign Key: dea_tk25
-  NULL::integer					AS kartennummer,
-  NULL::integer					AS tk25_ver,
+  NULL::integer		AS kartennummer,
+  NULL::integer		AS tk25_ver,
 -- I think this is our map as it is the only one with the name 'Bielefeld' - varify!
---  3917::integer					AS kartennummer,
---  1::integer					AS tk25_ver,
-  indeinl_uebergabestelle.kanalart::integer	AS kanal_art,		-- NOT NULL
-  indeinl_uebergabestelle.rechtswert::integer	AS rechtswert,
-  indeinl_uebergabestelle.hochwert::integer	AS hochwert
+--  3917::integer	AS kartennummer,
+--  1::integer		AS tk25_ver,
+  1::integer		AS kanal_art,		-- NOT NULL
+  NULL::integer		AS rechtswert,
+  NULL::integer		AS hochwert
 
-FROM auik.indeinl_uebergabestelle
+FROM auik.indeinl_genehmigung
   LEFT OUTER JOIN auik.basis_objekt
-    ON indeinl_uebergabestelle.objektid = basis_objekt.objektid
+    ON indeinl_genehmigung.objektid = basis_objekt.objektid
+  LEFT OUTER JOIN auik.basis_standort
+    ON basis_objekt.standortid = basis_standort.id
   LEFT OUTER JOIN auik.atl_klaeranlagen
-    ON indeinl_uebergabestelle.klaeranlage = atl_klaeranlagen.id
+    ON substring(basis_standort.entgebid from '[0-9]+')::integer = atl_klaeranlagen.id
 
 WHERE 
-  indeinl_uebergabestelle._deleted = FALSE AND
+  indeinl_genehmigung.anhang IS NOT NULL AND 
+  indeinl_genehmigung._deleted = FALSE AND
   basis_objekt._deleted = FALSE AND
+  basis_standort._deleted = FALSE AND
   basis_objekt.inaktiv = FALSE;
