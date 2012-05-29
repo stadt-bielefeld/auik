0eca1e69a78f5e3f8943f70530a3c0f0851d51af
Conny Pearce <Conny.Pearce@bielefeld.de>
2012-05-29 15:29:40 +0000

    A view for the calculated Analysepositions

---
+++ scripts/sql/auik_atl_analyseposition_all.sql
@@ -0,0 +1,28 @@
+﻿SELECT
+  *
+FROM
+  auik.atl_analyseposition
+
+UNION
+
+SELECT 
+  -bsb.id AS id, 
+  NULL::character varying(255) AS grkl, 
+  bsb.wert / csb.wert AS wert, 
+  NULL::character varying(255) AS analyse_von, 
+  NULL::character varying(255) AS bericht, 
+  NULL::double precision AS normwert, 
+  0::integer AS einheiten_id, 
+  'P00011'::character varying(255) AS parameter_id, 
+  bsb.probenahme_id, 
+  bsb._enabled, 
+  bsb._deleted
+FROM 
+  auik.atl_analyseposition bsb, 
+  auik.atl_analyseposition csb
+WHERE 
+  bsb.probenahme_id = csb.probenahme_id AND
+  bsb.parameter_id = 'L16250' AND 
+  csb.parameter_id = 'L15330' AND 
+  csb.wert IS NOT NULL  AND 
+  csb.wert != 0;
