33dcf1d261718215c0918583404f2e2879224bd4
Conny Pearce <Conny.Pearce@bielefeld.de>
2012-10-01 09:37:08 +0000

    Only transfer assays from 2001 onward.

--- data/scripts/sql/tipi_inka/version2.43_inka_probenahme.sql
+++ data/scripts/sql/tipi_inka/version2.43_inka_probenahme.sql
@@ -49,6 +49,7 @@ WHERE
   indeinl_genehmigung.anhang IS NOT NULL AND 
   indeinl_genehmigung.gen59 AND 
   basis_objekt.inaktiv = FALSE AND
+  atl_probenahmen.datum_der_entnahme >= '2001-01-01 00:00:00' AND
   indeinl_genehmigung._deleted = FALSE AND
   basis_objekt._deleted = FALSE AND
   view_two_way_objektverknuepfung._deleted = FALSE AND
