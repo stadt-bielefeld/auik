b506e2a14c9cd4dddca42c0d2cf5c084ceb011ee
Gerd Genuit <GerdGenuit@users.noreply.github.com>
2013-06-05 11:23:07 +0000

    Editor zum Bearbeiten der Einstellungen

---
+++ src/de/bielefeld/umweltamt/aui/utils/SortedProperties.java
@@ -0,0 +1,23 @@
+package de.bielefeld.umweltamt.aui.utils;
+
+
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.Vector;
+
+public class SortedProperties extends Properties {
+	/**
+	 * Overrides, called by the store method.
+	 */
+	@SuppressWarnings("unchecked")
+	public synchronized Enumeration keys() {
+		Enumeration keysEnum = super.keys();
+		Vector keyList = new Vector();
+		while (keysEnum.hasMoreElements()) {
+			keyList.add(keysEnum.nextElement());
+		}
+		Collections.sort(keyList);
+		return keyList.elements();
+	}
+}
