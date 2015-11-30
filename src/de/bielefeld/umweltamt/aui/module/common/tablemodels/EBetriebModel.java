7f5782a5a93cff456ec36bb94bde1f78d7cb4c08
Raimund Renkert <raimund.renkert@intevation.de>
2015-11-30 14:17:24 +0000

    Added tablemodel for syncable data types inthe ELKA module.

---
+++ src/de/bielefeld/umweltamt/aui/module/common/tablemodels/EBetriebModel.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright 2005-2011, Stadt Bielefeld
+ *
+ * This file is part of AUIK (Anlagen- und Indirekteinleiter-Kataster).
+ *
+ * AUIK is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * AUIK is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with AUIK. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * AUIK has been developed by Stadt Bielefeld and Intevation GmbH.
+ */
+package de.bielefeld.umweltamt.aui.module.common.tablemodels;
+
+import de.bielefeld.umweltamt.aui.mappings.elka.EBetrieb;
+import de.bielefeld.umweltamt.aui.utils.tablemodelbase.ListTableModel;
+
+public class EBetriebModel extends ListTableModel {
+
+    public EBetriebModel() {
+        super(new String[]{
+                "Anrede",
+                "Name",
+                "Strasse",
+                "Hausnummer",
+                "PLZ",
+                "Ort",
+                "Bezeichnung",
+                "selbstüberwachend"},
+                false,
+                true);
+    }
+
+	@Override
+	public void updateList() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object getColumnValue(Object objectAtRow, int columnIndex) {
+        Object value;
+
+        EBetrieb betrieb = (EBetrieb) objectAtRow;
+        switch(columnIndex) {
+            case 0:
+                value = betrieb.getStandort().getAdresse().getAnrede();
+                break;
+            case 1:
+            	value = betrieb.getStandort().getAdresse().getName1();
+            	break;
+            case 2:
+            	value = betrieb.getStandort().getAdresse().getStrasse();
+            	break;
+            case 3:
+            	value = betrieb.getStandort().getAdresse().getHausnr();
+            	break;
+            case 4:
+            	value = betrieb.getStandort().getAdresse().getPlzZst();
+            	break;
+            case 5:
+            	value = betrieb.getStandort().getAdresse().getOrtZst();
+            	break;
+            case 6:
+            	value = betrieb.getBezeichnung();
+            	break;
+            case 7:
+            	value = betrieb.getSuevkanTog() ? "ja" : "nein";
+            	break;
+            default:
+                value = null;
+        }
+
+        return value;
+	}
+
+}
---
+++ src/de/bielefeld/umweltamt/aui/module/common/tablemodels/EEinleitungsstelleModel.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright 2005-2011, Stadt Bielefeld
+ *
+ * This file is part of AUIK (Anlagen- und Indirekteinleiter-Kataster).
+ *
+ * AUIK is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * AUIK is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with AUIK. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * AUIK has been developed by Stadt Bielefeld and Intevation GmbH.
+ */
+package de.bielefeld.umweltamt.aui.module.common.tablemodels;
+
+import de.bielefeld.umweltamt.aui.mappings.elka.EEinleitungsstelle;
+import de.bielefeld.umweltamt.aui.utils.tablemodelbase.ListTableModel;
+
+public class EEinleitungsstelleModel extends ListTableModel {
+
+	public EEinleitungsstelleModel() {
+        super(new String[]{
+                "Standort-Nr",
+                "Bezeichnung",
+                "Typ"},
+                false,
+                true);
+    }
+
+	@Override
+	public void updateList() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object getColumnValue(Object objectAtRow, int columnIndex) {
+        Object value;
+
+        EEinleitungsstelle stelle = (EEinleitungsstelle) objectAtRow;
+        switch(columnIndex) {
+            case 0:
+          		value = stelle.getStandort().getAdresse().getName1();
+                break;
+            case 1:
+                value = stelle.getBezeichnung();
+                break;
+            case 2:
+                value = stelle.getTypIndirekteinleitungTog() ? "Indirekteinleiter" : "";
+                break;
+            default:
+                value = null;
+        }
+
+        return value;
+	}
+
+}
