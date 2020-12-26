c464d78730b9ff33b251541bac6f1ebc26e8a5f7
GerdGenuit <gerd.genuit@arcor.de>
2020-12-26 12:16:29 +0100

    show type of sonderbauwerk in objekt-list

--- src/de/bielefeld/umweltamt/aui/module/common/tablemodels/BasisObjektModel.java
+++ src/de/bielefeld/umweltamt/aui/module/common/tablemodels/BasisObjektModel.java
@@ -33,6 +33,7 @@ import de.bielefeld.umweltamt.aui.utils.StringUtils;
 import de.bielefeld.umweltamt.aui.utils.tablemodelbase.ListTableModel;
 import de.bielefeld.umweltamt.aui.mappings.basis.Standort;
 import de.bielefeld.umweltamt.aui.mappings.elka.Anfallstelle;
+import de.bielefeld.umweltamt.aui.mappings.oberflgw.Sonderbauwerk;
 import de.bielefeld.umweltamt.aui.module.BasisAdresseSuchen;
 
 /**
@@ -98,13 +99,48 @@ public class BasisObjektModel extends ListTableModel {
 				} else if (bo.getAnfallstelles().size() > 0) {
 					Set<Anfallstelle> list = bo.getAnfallstelles();
 					Anfallstelle anfallstelle = list.iterator().next();
-					if (anfallstelle.getAbwaBeschaffOpt() != null && anfallstelle.getAbwaBeschaffOpt() == 5) {
-						tmp = "Anfallstelle (Niederschlagswasser ohne Sonderbauwerk)";
+					if (anfallstelle.getNwHerBereichOpt() != null && anfallstelle.getNwHerBereichOpt() < 3) {
+						tmp = "Anfallstelle (NW nicht behandlungsbedürftig)";
+					} else if (anfallstelle.getNwHerBereichOpt() != null && anfallstelle.getNwHerBereichOpt() > 2) {
+						tmp = "Anfallstelle (NW behandlungsbedürftig)";
 					} else if (!anfallstelle.getAnhangId().equals("99")) {
 						tmp = "Anfallstelle (Anh " + anfallstelle.getAnhangId() + ")";
 					} else {
 						tmp = "Anfallstelle (" + anfallstelle.getAnlagenart() + ")";
 					}
+				} else if (bo.getSonderbauwerks().size() > 0) {
+					Set<Sonderbauwerk> list = bo.getSonderbauwerks();
+					Sonderbauwerk sonderbauwerk = bo.getSonderbauwerks().iterator().next();
+					if (sonderbauwerk.getTypOpt() == 1) {
+						tmp = "Sonderbauwerk (RRB)";
+						}
+					else if (sonderbauwerk.getTypOpt() == 2) {
+						tmp = "Sonderbauwerk (RKB)";
+						}
+					else if (sonderbauwerk.getTypOpt() == 3) {
+						tmp = "Sonderbauwerk (RBF)";
+						}
+					else if (sonderbauwerk.getTypOpt() == 4) {
+						tmp = "Sonderbauwerk (BF)";
+						}
+					else if (sonderbauwerk.getTypOpt() == 5) {
+						tmp = "Sonderbauwerk (RÜT)";
+						}
+					else if (sonderbauwerk.getTypOpt() == 6) {
+						tmp = "Sonderbauwerk (RÜM)";
+						}
+					else if (sonderbauwerk.getTypOpt() == 7) {
+						tmp = "Sonderbauwerk (RÜB)";
+						}
+					else if (sonderbauwerk.getTypOpt() == 8) {
+						tmp = "Sonderbauwerk (SK)";
+						}
+					else if (sonderbauwerk.getTypOpt() == 9) {
+						tmp = "Sonderbauwerk (RST)";
+						}
+					else {
+						tmp = "Sonderbauwerk (AL)";
+						}
 				} else {
 					tmp = bo.getObjektarten();
 				}
