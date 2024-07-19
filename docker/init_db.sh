4057017fd4df49e543cc0dfce0a54e804175ceb9
Alexander Woestmann <awoestmann@intevation.de>
2024-07-19 07:26:15 +0200

    Update init db script to use -q param for psql commands

--- docker/init_db.sh
+++ docker/init_db.sh
@@ -6,16 +6,16 @@ schema_file=$schema_dir/version1_0_schema.sql
 update_file_1_1_0=$schema_dir/updateTo_1_1_0.sql
 import_script=$schema_dir/import_csv.sql
 
-psql --command "CREATE DATABASE $DB_NAME;" >> /dev/null
-psql -d $DB_NAME --command "CREATE EXTENSION postgis;" >> /dev/null
+psql -q --command "CREATE DATABASE $DB_NAME;"
+psql -q -d $DB_NAME --command "CREATE EXTENSION postgis;"
 #Create user and assign roles
-psql -d $DB_NAME --command "CREATE USER ${USERNAME} with password '${USER_PW}';" >> /dev/null
+psql -q -d $DB_NAME --command "CREATE USER ${USERNAME} with password '${USER_PW}';"
 
 echo "Database: $DB_NAME"
 echo "Example user(password): ${USERNAME}(${USER_PW})"
 
 echo "Applying schema"
-psql -d $DB_NAME -f $schema_file >> /dev/null
-psql -d $DB_NAME -f $update_file_1_1_0 >> /dev/null
-psql -d $DB_NAME -f $import_script
+psql -q -d $DB_NAME -f $schema_file
+psql -q -d $DB_NAME -f $update_file_1_1_0
+psql -q -d $DB_NAME -f $import_script
 
