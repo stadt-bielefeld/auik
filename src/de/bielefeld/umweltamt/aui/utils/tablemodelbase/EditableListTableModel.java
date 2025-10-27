ecae03afec7bedcb941322f1e49976af81708b8c
Tom Gottfried <tom.gottfried@intevation.de>
2025-10-27 17:45:01 +0100

    Reduce code duplication
    The removed methods did nothing different than the overridden
    methods from the supertype.

--- src/de/bielefeld/umweltamt/aui/utils/tablemodelbase/EditableListTableModel.java
+++ src/de/bielefeld/umweltamt/aui/utils/tablemodelbase/EditableListTableModel.java
@@ -45,40 +45,6 @@ public abstract class EditableListTableModel extends ListTableModel {
     }
 
     /**
-     * Liefert das Objekt in einer bestimmten Zelle dieser Tabelle.
-     * @param rowIndex Die Tabellen-Zeile
-     * @param columnIndex Die Tabelle-Spalte
-     * @return Das Objekt in der Zelle (rowIndex, columnIndex) der Liste
-     * oder <code>null</code>, falls es die Zeile oder Spalte nicht gibt
-     */
-    @Override
-    public Object getValueAt(int rowIndex, int columnIndex) {
-        Object value = null;
-        if (getList() != null && rowIndex < getList().size()) {
-            if (columnIndex < columns.length) {
-                Object objectAtRow = getObjectAtRow(rowIndex);
-                value = getColumnValue(objectAtRow, columnIndex);
-            }
-        }
-        return value;
-    }
-
-    /**
-     * Liefert das Objekt in einer bestimmten Zeile der Tabelle,
-     * bzw. der Liste.
-     * @param rowIndex Die Zeile
-     * @return Das Objekt in der Zeile rowIndex oder <code>null</code>, falls die Zeile nicht existiert
-     */
-    @Override
-    public Object getObjectAtRow(int rowIndex) {
-        if (rowExists(rowIndex)) {
-            return super.getObjectAtRow(rowIndex);
-        } else {
-            return null;
-        }
-    }
-
-    /**
      * Liefert die Anzahl Zeilen in der Liste + 1. Damit wird
      * sichergestellt, dass immer eine Leerzeile zum anlegen
      * einer neuen Zeile zur Verfügung steht.
